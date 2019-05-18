17659e14173f9efa0ee11606a1681954390a2e3e
Nathaniel Vala <nathanielvala@hotmail.com>
2019-05-18 10:45:22 +1000

    add properties

--- Atlas/TagEntity/SPYDER/nifi-addAtlasTag-processors/src/main/java/spyder/processors/addAtlasTag/MyProcessor.java
+++ Atlas/TagEntity/SPYDER/nifi-addAtlasTag-processors/src/main/java/spyder/processors/addAtlasTag/MyProcessor.java
@@ -40,38 +40,106 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-@Tags({"example"})
-@CapabilityDescription("Provide a description")
+
+
+/* Code written by spyder by sampling existing nifi processors */
+import org.apache.nifi.annotation.behavior.InputRequirement;
+import org.apache.nifi.annotation.behavior.InputRequirement.Requirement;
+import org.apache.nifi.expression.ExpressionLanguageScope;
+
+@InputRequirement(Requirement.INPUT_REQUIRED)
+@Tags({"MetaData", "Meta Data", "Post", "Atlas"})
+
+
+@CapabilityDescription("Finds entity in Atlas and associates a Tag to the it")
 @SeeAlso({})
-@ReadsAttributes({@ReadsAttribute(attribute="", description="")})
+@ReadsAttributes({@ReadsAttribute(attribute="metadataTags", description="A JSON structured as per Atlas Classifications for assignment of Tags through the")})
 @WritesAttributes({@WritesAttribute(attribute="", description="")})
+
+
+
 public class MyProcessor extends AbstractProcessor {
 
-    public static final PropertyDescriptor MY_PROPERTY = new PropertyDescriptor
-            .Builder().name("MY_PROPERTY")
-            .displayName("My property")
-            .description("Example Property")
-            .required(true)
-            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
-            .build();
+    //@TODO: Make more Properties as required
+    static final PropertyDescriptor METADATA = new PropertyDescriptor.Builder()
+        .name("MetaData")
+        .description("JSON of the metadata needed to be attached")
+        .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+        .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
+        .defaultValue("${metadataTags}")
+        .required(true)
+        .build();
 
-    public static final Relationship MY_RELATIONSHIP = new Relationship.Builder()
-            .name("MY_RELATIONSHIP")
-            .description("Example relationship")
-            .build();
+        static final PropertyDescriptor QUALIFIED_NAME = new PropertyDescriptor.Builder()
+        .name("Qualified Name")
+        .description("The Qualified Name of the Entity wanting to tag")
+        .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+        .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
+        .defaultValue("${qualifiedname}")
+        .required(true)
+        .build();
 
+        static final PropertyDescriptor ATLAS_URL = new PropertyDescriptor.Builder()
+        .name("Atlas Base URL (<PROTOCOL>://<HOST>:<PORT>")
+        .description("The URL for the Atlas API (base url) ending ")
+        .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+        .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
+        .defaultValue("${qualifiedname}")
+        .required(true)
+        .build();
+
+        static final PropertyDescriptor ATLAS_USER = new PropertyDescriptor.Builder()
+        .name("Atlas User")
+        .description("The Atlas User for setting Metadata")
+        .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+        .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
+        .defaultValue("${qualifiedname}")
+        .required(true)
+        .build();
+
+        // @TODO: Make Password a sensitve property
+        static final PropertyDescriptor ATLAS_PASSWORD = new PropertyDescriptor.Builder()
+        .name("Qualified Name")
+        .description("The password for Atlas User")
+        .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+        .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
+        .defaultValue("")
+        .required(true)
+        .build();
+
+        static final PropertyDescriptor NUM_OF_RETRIES = new PropertyDescriptor.Builder()
+        .name("Number of retries")
+        .description("The number of retries before failing the flowfile")
+        .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
+        .defaultValue("30")
+        .required(false)
+        .build();
+
+    //@TODO: Make more relationships for non-sucessfull flows
+    static final Relationship REL_SUCCESS = new Relationship.Builder()
+        .name("success")
+        .description("Any FlowFile that successfully tags the JSON MetaData to Atlas Entity referenced in the Attributes")
+        .build();
+    
+    //@TODO: Add all Properties to the this variable via below method
     private List<PropertyDescriptor> descriptors;
 
+    //@TODO: Add all Relationships to the this variable via below method
     private Set<Relationship> relationships;
 
     @Override
     protected void init(final ProcessorInitializationContext context) {
         final List<PropertyDescriptor> descriptors = new ArrayList<PropertyDescriptor>();
-        descriptors.add(MY_PROPERTY);
+        descriptors.add(METADATA);
+        descriptors.add(QUALIFIED_NAME);
+        descriptors.add(ATLAS_URL);
+        descriptors.add(ATLAS_USER);
+        descriptors.add(ATLAS_PASSWORD);
+        descriptors.add(NUM_OF_RETRIES);
         this.descriptors = Collections.unmodifiableList(descriptors);
 
         final Set<Relationship> relationships = new HashSet<Relationship>();
-        relationships.add(MY_RELATIONSHIP);
+        relationships.add(REL_SUCCESS);
         this.relationships = Collections.unmodifiableSet(relationships);
     }
 
@@ -96,6 +164,7 @@ public class MyProcessor extends AbstractProcessor {
         if ( flowFile == null ) {
             return;
         }
-        // TODO implement
+        // @TODO: Get the GUID of an entity and set the Attribute
+        // @TODO: Implement the post to atlas here (ensure the loop happens for slower Atals integrations)
     }
 }
